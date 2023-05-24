473d58d12d73b1a43ce89bfc9d149c1f65b3d21e
PACY2 <cmouhrach@gmail.com>
2023-05-24 11:38:14 +0000

    table created

--- packages/ui/src/Components/Table/index.tsx
+++ packages/ui/src/Components/Table/index.tsx
@@ -41,7 +41,7 @@ export const Table = <T extends {}>({
   setPagination,
   isLoading,
   handleEdit,
-  handleDelete
+  handleDelete,
 }: TableProps<T>) => {
   const [sorting, setSorting] = useState<SortingState>([]);
 
@@ -70,132 +70,138 @@ export const Table = <T extends {}>({
   return (
     <div className="flex flex-col gap-4">
       <Input placeholder="Search..." onChange={handleSearch} />
-      <div className="border rounded-md">
-        <table className="w-full ">
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <th
-                      className="text-start p-3 font-semibold tracking-wide"
-                      key={header.id}
-                      colSpan={header.colSpan}
-                    >
-                      {header.isPlaceholder ? null : (
-                        <div
-                          {...{
-                            className: header.column.getCanSort()
-                              ? "cursor-pointer select-none flex items-center gap-2 "
-                              : "",
-                            onClick: header.column.getToggleSortingHandler(),
-                          }}
-                        >
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                          {{
-                            asc: <MdOutlineChevronLeft className="rotate-90" />,
-                            desc: (
-                              <MdOutlineChevronLeft className="-rotate-90" />
-                            ),
-                          }[header.column.getIsSorted() as string] ?? null}
-                        </div>
-                      )}
+      <div className="w-full overflow-x-scroll">
+        <div className="border rounded-md w-fit lg:w-full">
+          <table className="w-full table-auto">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <th
+                        className="text-start p-3 font-semibold tracking-wide"
+                        key={header.id}
+                        colSpan={header.colSpan}
+                      >
+                        {header.isPlaceholder ? null : (
+                          <div
+                            {...{
+                              className: header.column.getCanSort()
+                                ? "cursor-pointer select-none flex items-center gap-2 "
+                                : "",
+                              onClick: header.column.getToggleSortingHandler(),
+                            }}
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            {{
+                              asc: (
+                                <MdOutlineChevronLeft className="rotate-90" />
+                              ),
+                              desc: (
+                                <MdOutlineChevronLeft className="-rotate-90" />
+                              ),
+                            }[header.column.getIsSorted() as string] ?? null}
+                          </div>
+                        )}
+                      </th>
+                    );
+                  })}
+                  {(handleEdit || handleDelete) && (
+                    <th className="text-start p-3 font-semibold tracking-wide">
+                      Actions
                     </th>
-                  );
-                })}
-                {
-                  (handleEdit || handleDelete) && (
-                    <th
-                      className="text-start p-3 font-semibold tracking-wide"
-                    >Actions</th>
-                  )
-                }
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {
-              !isLoading &&
-              table.getRowModel().rows.map((row) => (
-                <tr className="odd:bg-stone-100" key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                    <td className="p-3 tracking-wide" key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </td>
-                  ))}
-                  {
-                    (handleEdit || handleDelete) && (
-                      <td className="p-3 tracking-wide" >
-                        <Actions handleDelete={handleDelete} handleEdit={handleEdit} id={row.getValue("id")} />
+                  )}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {!isLoading &&
+                table.getRowModel().rows.map((row) => (
+                  <tr className="odd:bg-stone-100" key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <td className="p-3 tracking-wide" key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
                       </td>
-                    )
-                  }
-                </tr>
-              ))
-            }
-            {
-              isLoading && [1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
-                <tr key={index} className="border-b-2 border-white" >
-                  <td className={`p-3 bg-stone-200 animate-pulse`} colSpan={columns.length} >
-                    &nbsp;
-                  </td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </div>
-      {
-        !isLoading && (
-          <div className="flex items-center justify-between gap-2">
-            <span className="flex items-center gap-1">
-              <div>Page</div>
-              <strong>
-                <input
-                  type="number"
-                  value={table.getState().pagination.pageIndex + 1}
-                  onChange={(e) => {
-                    const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                    table.setPageIndex(page);
-                  }}
-                  className="border p-1 rounded w-16"
-                />
-                of {table.getPageCount()}
-              </strong>
-            </span>
-            <div className="flex items-center gap-2">
-              <Button
-                onClick={() => table.setPageIndex(0)}
-                disabled={!table.getCanPreviousPage()}
-              >
-                <MdOutlineKeyboardDoubleArrowLeft className="fill-white text-xl" />
-              </Button>
-              <Button
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-              >
-                <MdOutlineKeyboardArrowLeft className="fill-white text-xl" />
-              </Button>
-              <Button
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-              >
-                <MdOutlineKeyboardArrowRight className="fill-white text-xl" />
-              </Button>
-              <Button
-                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                disabled={!table.getCanNextPage()}
-              >
-                <MdOutlineKeyboardDoubleArrowRight className="fill-white text-xl" />
-              </Button>
+                    ))}
+                    {(handleEdit || handleDelete) && (
+                      <td className="p-3 tracking-wide">
+                        <Actions
+                          handleDelete={handleDelete}
+                          handleEdit={handleEdit}
+                          id={row.getValue("id")}
+                        />
+                      </td>
+                    )}
+                  </tr>
+                ))}
+              {isLoading &&
+                [1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
+                  <tr key={index} className="border-b-2 border-white">
+                    <td
+                      className={`p-3 bg-stone-200 animate-pulse`}
+                      colSpan={columns.length}
+                    >
+                      &nbsp;
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          {!isLoading && (
+            <div className="p-3 flex items-center justify-between gap-2">
+              <span className="flex items-center gap-1">
+                <div>Page</div>
+                <strong>
+                  <input
+                    type="number"
+                    value={table.getState().pagination.pageIndex + 1}
+                    onChange={(e) => {
+                      const page = e.target.value
+                        ? Number(e.target.value) - 1
+                        : 0;
+                      table.setPageIndex(page);
+                    }}
+                    className="border p-1 rounded w-16"
+                  />
+                  of {table.getPageCount()}
+                </strong>
+              </span>
+              <div className="flex items-center gap-2">
+                <Button
+                  onClick={() => table.setPageIndex(0)}
+                  disabled={!table.getCanPreviousPage()}
+                >
+                  <MdOutlineKeyboardDoubleArrowLeft className="fill-white text-xl" />
+                </Button>
+                <Button
+                  onClick={() => table.previousPage()}
+                  disabled={!table.getCanPreviousPage()}
+                >
+                  <MdOutlineKeyboardArrowLeft className="fill-white text-xl" />
+                </Button>
+                <Button
+                  onClick={() => table.nextPage()}
+                  disabled={!table.getCanNextPage()}
+                >
+                  <MdOutlineKeyboardArrowRight className="fill-white text-xl" />
+                </Button>
+                <Button
+                  onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                  disabled={!table.getCanNextPage()}
+                >
+                  <MdOutlineKeyboardDoubleArrowRight className="fill-white text-xl" />
+                </Button>
+              </div>
             </div>
-          </div>
-
-        )
-      }
+          )}
+        </div>
+      </div>
     </div>
   );
 };
@@ -207,32 +213,30 @@ interface ActionsProps {
 }
 
 const Actions = ({ id, handleDelete, handleEdit }: ActionsProps) => {
-
-  const [isDeletingLoading, setIsDeletingLoading] = useState(false)
+  const [isDeletingLoading, setIsDeletingLoading] = useState(false);
 
   const handleDeleteWrapper = async () => {
-    setIsDeletingLoading(true)
-    if (handleDelete) await handleDelete(id)
-    setIsDeletingLoading(false)
-  }
+    setIsDeletingLoading(true);
+    if (handleDelete) await handleDelete(id);
+    setIsDeletingLoading(false);
+  };
 
   return (
-    <div className="flex items-center gap-2" >
-      {
-        handleEdit && (
-          <Button onClick={() => handleEdit(id)} color="success" >
-            Edit
-          </Button>
-        )
-      }
-      {
-        handleDelete && (
-          <Button isLoading={isDeletingLoading} onClick={() => handleDeleteWrapper()} color="danger" >
-            Delete
-          </Button>
-        )
-      }
+    <div className="flex items-center gap-2">
+      {handleEdit && (
+        <Button onClick={() => handleEdit(id)} color="success">
+          Edit
+        </Button>
+      )}
+      {handleDelete && (
+        <Button
+          isLoading={isDeletingLoading}
+          onClick={() => handleDeleteWrapper()}
+          color="danger"
+        >
+          Delete
+        </Button>
+      )}
     </div>
-
-  )
-}
+  );
+};
